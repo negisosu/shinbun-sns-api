397ad8ce64c1c4713bb4b3ddc79345f2abbf7e98
tatsu <donnguri0404@gmail.com>
2025-05-21 11:25:12 +0900

    supabaseのtokenでの疎通確認用

--- index.ts
+++ index.ts
@@ -10,8 +10,8 @@ const port = 3002
 app.use(express.json())
 //Flutterから正しいaccessTokenを送られてきてるかを判定するmiddleware,自分でapi構築する時は使用しない
 app.use(authenticate)
-app.use("/users", userRoutes)
-app.use("/news-contents", newsContentRoutes)
+// app.use("/users", userRoutes)
+// app.use("/news-contents", newsContentRoutes)
 
 app.get("/", (req: express.Request, res: express.Response) => {
     res.status(200).json({ message: "Authentication successful" })
